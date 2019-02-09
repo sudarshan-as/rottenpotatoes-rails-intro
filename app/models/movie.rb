87790d7dc72f5d9f8b054f5ae930d47d34097827
EC2 Default User <ec2-user@ip-172-31-38-94.us-east-2.compute.internal>
2019-02-09 18:56:08 +0000

    Sort by title and updates to the movies.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
     
     def sort_by_title
         #change = ['cents', 'pennies', 'coins', 'dimes', 'pence', 'quarters']
-        movie.sort_by { |movie| movie }
+        movie.sort_by { |title| title }
     end
 
     def sort_by_date
