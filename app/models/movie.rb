0b9b17940f090d5dab512c63b56f4715e48a4a71
EC2 Default User <ec2-user@ip-172-31-38-94.us-east-2.compute.internal>
2019-02-09 18:52:25 +0000

    Link to and sort by title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+    
+    def sort_by_title
+        #change = ['cents', 'pennies', 'coins', 'dimes', 'pence', 'quarters']
+        movie.sort_by { |movie| movie }
+    end
+
+    def sort_by_date
+        
+    end
 end
